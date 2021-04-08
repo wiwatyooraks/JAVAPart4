4662ca89e9bbe4ff54fec0c55f009febadeb165a
wiwatyooraks <78940910+wiwatyooraks@users.noreply.github.com>
2021-04-08 21:56:23 +0700

    JAVAPart4

---
+++ Abstract.java
@@ -0,0 +1,11 @@
+package com.wiwatyooraks.javapart3;
+
+abstract class Abstract {
+	public int i=1;
+	public abstract void move();
+	public abstract void eat();
+
+	public void stand() {
+	System.out.println("Stand Still...");
+	}
+}
---
+++ Primitive.java
@@ -0,0 +1,18 @@
+package com.wiwatyooraks.javapart3;
+
+class PrimitiveConversion {
+	public static void main(String[] args) {
+		int myInt = 5;
+		float myFloat = myInt;
+		System.out.println("myInt value: " + myInt);
+		System.out.println("myFloat value: " + myFloat);
+		float newFloat = 5.5f;
+		int newInt = (int) newFloat;
+		System.out.println("newFloat value: " + newFloat);
+		System.out.println("newInt value: " + newInt);
+		int otherInt = 130; 
+		byte myByte = (byte) otherInt;
+		System.out.println("otherInt value: " + otherInt); 
+		System.out.println("myByte value: " + myByte); 
+	}
+}
